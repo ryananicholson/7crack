388a5e97b95b0c4b522d6803041224157e441908
Ryan Nicholson <nicholsr@ragnar.hsd1.pa.comcast.net>
2019-07-07 18:16:16 -0400

    Final verison

--- 7crack.py
+++ 7crack.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
 
+# 7crack.py
+# Description: Cracks Cisco Type 7 passwords
+# Author: Ryan Nicholson (@ryananicholson)
+# 
+# Example: python 7crack.py --hash 095C4F1A0A1218000F
+
 import argparse
 
 def crackpassword(hashval):
-	crypttext = hashval.upper()
-	print(crypttext)
+	constant = "tfd;kfoA,.iyewrkldJKD"
+	ciphertext = hashval.upper()
+	seed = int(ciphertext[0] + ciphertext[1])
+	plaintext = ""
+
+	for i in range(1, len(ciphertext) / 2):
+		salt = constant[seed - 1]
+		hexval = int(ciphertext[i * 2] + ciphertext[i * 2 + 1], 16)
+		plaintext += chr(hexval ^ ord(salt))
+		seed += 1
+
+	print("Plaintext password is: " + plaintext)
 
 # Argument handling
 parser = argparse.ArgumentParser()
